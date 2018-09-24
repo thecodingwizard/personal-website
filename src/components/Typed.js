90db3cd09c0a6556590c0e2b4b8cbd27ba88f853
nw_thecodingwizard <nathan.r.wang@gmail.com>
2018-09-24 21:13:38 +0000

    fix: use settimeout instead of setinterval for typed

--- src/components/Typed.js
+++ src/components/Typed.js
@@ -7,83 +7,92 @@ let transition = 800;
 let time = 2000;
 
 const styles = theme => ({
-	fade: {
-		opacity: 1,
-		transition: `opacity ${transition-10}ms`
-	},
-	visible: {
-		opacity: 1,
-	},
-	hidden: {
-		opacity: 0,
-	},
+  fade: {
+    opacity: 1,
+    transition: `opacity ${transition-10}ms`
+  },
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
 });
 
 class Typed extends Component {
-	state = {
-		current: 0,
-		isVisible: false,
-	};
+  state = {
+    current: 0,
+    isVisible: false,
+  };
+  _isMounted = true;
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.isMobile = new MobileDetect(window.navigator.userAgent).mobile();
+    this.isMobile = new MobileDetect(window.navigator.userAgent).mobile();
 
-		this.el = React.createRef();
-	}
+    this.el = React.createRef();
+  }
 
-	componentDidMount() {
-		if (this.isMobile) {
-			setTimeout(() => this.setState({ isVisible: true }), 100);
-			this.fade = setInterval(this.handleFade, 2*transition+time);
-		} else {
-			const options = {
-				strings: this.props.strings,
-				typeSpeed: 30,
-				backSpeed: 20,
-				backDelay: 2000,
-				loop: true,
-				smartBackspace: true,
-			};
-	
-			this.typed = new TypedJS(this.el.current, options);
-		}
-	}
+  componentDidMount() {
+    if (this.isMobile) {
+      setTimeout(() => {
+        this.setState({ isVisible: true });
+        setTimeout(() => {
+          this.handleFade();
+        }, transition + time);
+      }, 100);
+    } else {
+      const options = {
+        strings: this.props.strings,
+        typeSpeed: 30,
+        backSpeed: 20,
+        backDelay: 2000,
+        loop: true,
+        smartBackspace: true,
+      };
+  
+      this.typed = new TypedJS(this.el.current, options);
+    }
+  }
 
-	componentWillUnmount() {
-		if (this.typed) this.typed.destroy();
-		if (this.fade) clearInterval(this.fade);
-	}
-	
-	handleFade = () => {
-		this.setState({
-			isVisible: false
-		});
-		setTimeout(() => {
-			this.setState(state => ({
-				current: (state.current+1)%this.props.strings.length,
-				isVisible: true,
-			}));
-		}, transition);
-	}
+  componentWillUnmount() {
+    if (this.typed) this.typed.destroy();
+    this._isMounted = false;
+  }
+  
+  handleFade = () => {
+    if (!this._isMounted) return;
+    
+    this.setState({
+      isVisible: false
+    });
+    setTimeout(() => {
+      this.setState(state => ({
+        current: (state.current+1)%this.props.strings.length,
+        isVisible: true,
+      }));
+      
+      setTimeout(this.handleFade, transition + time);
+    }, transition);
+  }
 
-	render() {
-		const { classes, strings } = this.props;
-		const { isVisible, current } = this.state;
+  render() {
+    const { classes, strings } = this.props;
+    const { isVisible, current } = this.state;
 
-		return (
-			<div>
-				{!this.isMobile && <span ref={this.el} />}
-				{
-					this.isMobile &&
-					<span className={`${classes.fade} ${isVisible ? classes.visible : classes.hidden}`}>
-						{strings[current]}
-					</span>
-				}
-			</div>
-		);
-	}
+    return (
+      <div>
+        {!this.isMobile && <span ref={this.el} />}
+        {
+          this.isMobile &&
+          <span className={`${classes.fade} ${isVisible ? classes.visible : classes.hidden}`}>
+            {strings[current]}
+          </span>
+        }
+      </div>
+    );
+  }
 }
 
 export default injectSheet(styles)(Typed);
