94fb017c13af90fc30de5bf827524560e1b2201d
Nathan Wang <nathan.r.wang@gmail.com>
2019-01-13 21:17:25 -0800

    wip: about page

--- src/containers/about/WebTechnologies.js
+++ src/containers/about/WebTechnologies.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import injectSheet from "react-jss";
+import angularLogo from "./angular.svg";
+import reactLogo from "./react.svg";
+import bootstrapLogo from "./bootstrap.svg";
+import pwaLogo from "./pwa.svg";
+import html5Logo from "./html5.svg";
 
 const styles = theme => ({
-  root: {
+  lightBgContainer: {
     backgroundColor: "#F0F4F8",
-    padding: "3.5rem",
+    padding: "5rem 1rem",
   },
   title: {
     fontSize: "3rem",
     fontWeight: "300",
     fontFamily: "Roboto",
     marginTop: 0,
+    marginBottom: "1rem",
+  },
+  blurb: {
+    fontSize: "1.25rem",
+  },
+  logoRow: {
+    textAlign: "center",
+  },
+  logo: {
+    width: "200px",
+    height: "200px",
+    margin: "10px",
+  },
+  logoBig: {
+    width: "170px",
+    height: "170px",
+    margin: "25px",
   },
 });
 
@@ -18,12 +40,22 @@ class WebTechnologies extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.root}>
-        <div className="container">
-          <h2 className={classes.title}>Front End Web Development</h2>
-          <p>I specialize in front-end web development, building complex Single-Page Applications.</p>
+      <React.Fragment>
+        <div className={classes.lightBgContainer}>
+          <div className="container">
+            <h2 className={classes.title}>Front End Web Development</h2>
+            <p className={classes.blurb}>I specialize in front-end web development, building complex Single-Page
+              Applications.</p>
+            <div className={classes.logoRow}>
+              <img src={angularLogo} className={classes.logo} alt="Angular" />
+              <img src={pwaLogo} className={classes.logoBig} alt="Progressive Web Apps" />
+              <img src={reactLogo} className={classes.logo} alt="React" />
+              <img src={bootstrapLogo} className={classes.logo} alt="Bootstrap" />
+              <img src={html5Logo} className={classes.logoBig} alt="HTML 5" />
+            </div>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
