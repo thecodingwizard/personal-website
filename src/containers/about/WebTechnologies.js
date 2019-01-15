03fd1846835c824cae3621441e480103711ac2be
Nathan Wang <nathan.r.wang@gmail.com>
2019-01-14 21:58:43 -0800

    fix image size on mobile

--- src/containers/about/WebTechnologies.js
+++ src/containers/about/WebTechnologies.js
@@ -31,7 +31,7 @@ const styles = theme => ({
       flex: 1,
       "& img": {
         width: "100%",
-        minWidth: "100px",
+        minWidth: "30px",
       },
     },
   },
@@ -54,14 +54,18 @@ class WebTechnologies extends Component {
             <h2 className={classes.title}>Front End Development</h2>
             <p className={classes.blurb}>I specialize in front-end web development, building complex Single-Page
               Applications.</p>
+            {/*<div className={classes.tech}>*/}
+              {/*<img src="/assets/angular.svg" alt="Angular" />*/}
+              {/**/}
+            {/*</div>*/}
             <div className={classes.logoRow}>
-              <div><img src="/assets/angular.svg" className={classes.logo} alt="Angular" /></div>
-              <div><img src="/assets/pwa.svg" className={classes.logoBig} alt="Progressive Web Apps" /></div>
-              <div><img src="/assets/react.svg" className={classes.logo} alt="React" /></div>
+              <div><img src="/assets/angular.svg" alt="Angular" /></div>
+              <div><img src="/assets/pwa.svg" alt="Progressive Web Apps" /></div>
+              <div><img src="/assets/react.svg" alt="React" /></div>
               <span className={classes.logoBreak}></span>
-              <div><img src="/assets/bootstrap.svg" className={classes.logo} alt="Bootstrap" /></div>
-              <div><img src="/assets/html5.svg" className={classes.logoBig} alt="HTML 5" /></div>
-              <div><img src="/assets/css3.svg" className={classes.logoBig} alt="CSS 3" /></div>
+              <div><img src="/assets/bootstrap.svg" alt="Bootstrap" /></div>
+              <div><img src="/assets/html5.svg" alt="HTML 5" /></div>
+              <div><img src="/assets/css3.svg" alt="CSS 3" /></div>
             </div>
           </div>
         </div>
