304fe795b501c1709511b6271b9107ceccae7b38
Nathan Wang <nathan.r.wang@gmail.com>
2020-12-17 14:01:42 -0800

    update homepage text

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Head from 'next/head'
 import classNames from "classnames";
-import Typed from 'react-typed';
 import 'react-typed/dist/animatedCursor.css';
 import Particles from "../components/Particles";
-
-const descriptionStrings = [
-  `Programmer`,
-  `2x USACO Finalist.`,
-  `Board Game Enthusiast`,
-  `Web App Developer`,
-  `4x AIME Qualifier`,
-];
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(undefined);
@@ -30,21 +21,19 @@ export default function Home() {
         {isMobile !== undefined ?
           <Particles
             canvasClassName="block"
-            className={classNames("absolute inset-0 -z-10 bg-gradient-to-br from-blue-800 to-blue-900")}
+            className={classNames("absolute inset-0 -z-10 bg-blue-900")}
             isMobile={isMobile} /> : null}
-        <div className="py-8 px-6 md:px-12 lg:px-24 max-w-6xl w-full mx-auto">
-          <span className="text-xl sm:text-2xl xl:text-4xl font-semibold text-accent">Hi there! I'm</span>
+        <div className="py-8 px-6 md:px-12 lg:px-24 max-w-7xl w-full mx-auto">
+          <span className="text-xl sm:text-2xl font-semibold text-accent">Hi there! I'm</span>
           <div className="lg:h-4"/>
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-9xl text-white font-semibold lg:-ml-1 xl:-ml-1">Nathan Wang<span className="text-accent">.</span></h1>
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl text-white font-semibold lg:-ml-1 xl:-ml-1">Nathan Wang<span className="text-accent">.</span></h1>
           <div className="h-4 sm:h-6 md:h-8 xl:h-16" />
-          <p className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-blue-100 xl:-ml-0.5">
-            I'm a{' '}
-            <Typed
-              strings={descriptionStrings.map(s => `${s}^1000`)}
-              typeSpeed={40}
-              backSpeed={20}
-              loop />
-          </p>
+          <div className="text-blue-200 text-xl max-w-prose leading-relaxed space-y-4">
+            <p>
+              I love programming, especially competitive programming and web development.
+              I also play a lot of board games, especially social deduction games like Secret Hitler!
+            </p>
+          </div>
           <div className="h-12 sm:h-16 lg:h-24 xl:h-36" />
           <div className="font-mono text-blue-300 sm:text-lg md:text-xl font-semibold">
             <a className="text-accent hover:underline" href="mailto:nathan.r.wang@gmail.com" target="_blank">Email</a> · <a className="text-accent hover:underline" href="https://github.com/thecodingwizard" target="_blank">Github</a> · <a className="text-accent hover:underline" href="https://www.linkedin.com/in/nathan-r-wang/" target="_blank">Linkedin</a>
