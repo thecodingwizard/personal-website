fec910655a4c350f453c1d39f9adb55090866814
Nathan Wang <nathan.r.wang@gmail.com>
2021-08-14 21:57:40 +0000

    games

--- src/lib/CardGrid/Card.svelte
+++ src/lib/CardGrid/Card.svelte
@@ -1,20 +1,57 @@
 <script>
-  export let title, link, tags;
+  import AES from "crypto-js/aes.js";
+  import Utf8 from "crypto-js/enc-utf8.js";
+
+  export let title, link = undefined, tags = undefined, encrypted = false;
+
+  const handleClick = () => {
+    const password = prompt("Enter link password:");
+    const decrypted = AES.decrypt(link, password).toString(Utf8);
+
+    if (decrypted) {
+      window.location.href = decrypted;
+    } else {
+      alert("Incorrect password");
+    }
+  }
 </script>
 
 {#if link}
-  <a href={link} target="_blank"
-     class="flex flex-col bg-blue-900 bg-opacity-[15%] hover:bg-opacity-25 group p-6 shadow-md">
-    <h2 class="text-blueGray-300 font-mono font-bold text-xl mb-4 group-hover:text-accent">{title}</h2>
-    <p class="text-blueGray-400 group-hover:text-blueGray-200 flex-1">
-      <slot></slot>
-    </p>
-    {#if tags}
-      <div class="mt-6 text-sm text-blueGray-500 group-hover:text-blueGray-400 font-mono">
-        {tags.join(` · `)}
-      </div>
-    {/if}
-  </a>
+  {#if encrypted}
+    <button
+      on:click={handleClick}
+      class="flex flex-col bg-blue-900 bg-opacity-[15%] hover:bg-opacity-25 group p-6 shadow-md text-left">
+      <h2 class="text-blueGray-300 font-mono font-bold text-xl group-hover:text-accent">{title}</h2>
+      {#if $$slots.default}
+        <div class="h-4" />
+        <p class="text-blueGray-400 group-hover:text-blueGray-200 flex-1">
+          <slot></slot>
+        </p>
+      {/if}
+      {#if tags}
+        <div class="mt-6 text-sm text-blueGray-500 group-hover:text-blueGray-400 font-mono">
+          {tags.join(` · `)}
+        </div>
+      {/if}
+    </button>
+  {:else}
+    <a href={link}
+      target={link.startsWith("/") ? undefined : "_blank"}
+      class="flex flex-col bg-blue-900 bg-opacity-[15%] hover:bg-opacity-25 group p-6 shadow-md">
+      <h2 class="text-blueGray-300 font-mono font-bold text-xl group-hover:text-accent">{title}</h2>
+      {#if $$slots.default}
+        <div class="h-4" />
+        <p class="text-blueGray-400 group-hover:text-blueGray-200 flex-1">
+          <slot></slot>
+        </p>
+      {/if}
+      {#if tags}
+        <div class="mt-6 text-sm text-blueGray-500 group-hover:text-blueGray-400 font-mono">
+          {tags.join(` · `)}
+        </div>
+      {/if}
+    </a>
+  {/if}
 {:else}
   <div class="flex flex-col bg-blue-900 bg-opacity-[15%] p-6 shadow-md">
     <h2 class="text-blueGray-300 font-mono font-bold text-xl mb-4">{title}</h2>
--- src/lib/ProjectsList.svelte
+++ src/lib/ProjectsList.svelte
@@ -13,15 +13,19 @@
     promoting competitive programming amongst students.
   </Card>
   <Card title="Competitive Programming Solutions"
-                link="https://github.com/thecodingwizard/competitive-programming/" tags={["Competitive Programming"]}>
+        link="https://github.com/thecodingwizard/competitive-programming/" tags={["Competitive Programming"]}>
     This Github repository contains solutions to some of the 900+ competitive programming problems I've solved!
   </Card>
   <Card title="Realtime IDE"
-                link="https://github.com/cpinitiative/ide" tags={["Vite", "React", "Firebase"]}>
+        link="https://github.com/cpinitiative/ide" tags={["Vite", "React", "Firebase"]}>
     A realtime collaborative IDE with code execution and input/output, designed for competitive programming and USACO.
   </Card>
   <Card title="Personal Website"
-                link="https://github.com/thecodingwizard/personal-website" tags={["Svelte", "SvelteKit", "Tailwind"]}>
+        link="https://github.com/thecodingwizard/personal-website" tags={["Svelte", "SvelteKit", "Tailwind"]}>
     This site! Built with Svelte, SvelteKit, and Tailwind CSS.
+  </Card>
+  <Card title="Party Games"
+        link="/games" tags={["Miscellaneous"]}>
+    A collection of my favorite party games.
   </Card>
 </CardGrid>
