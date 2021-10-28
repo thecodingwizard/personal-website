f057936bcc6308f09bc95d6ea475f2416f918ff6
Nathan Wang <nathan.r.wang@gmail.com>
2021-10-27 21:05:24 -0700

    remove old svelte files

--- src/lib/CardGrid/CardGrid.svelte
+++
@@ -1,3 +0,0 @@
-<div class="grid md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8">
-  <slot></slot>
-</div>
--- src/lib/LinkToSection.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let title, href;
-</script>
-
-<a href="#{href}" class="block sm:text-lg lg:text-xl max-w-prose leading-relaxed group">
-  <span class="group-hover:text-fuchsia-300 text-accent uppercase font-mono">{title}</span><br/>
-  <p class="group-hover:text-blueGray-300 text-blueGray-400"><slot></slot></p>
-</a>
