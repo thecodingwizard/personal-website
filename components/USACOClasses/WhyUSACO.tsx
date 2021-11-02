d9d7d1f666e28999d1e9d99d549c65b8793d86db
Nathan Wang <nathan.r.wang@gmail.com>
2021-11-01 17:11:06 -0700

    feat: move why usaco into an expandable block

---
+++ components/USACOClasses/WhyUSACO.tsx
@@ -0,0 +1,72 @@
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
+
+export default function WhyUSACO() {
+	const [show, setShow] = useState(false);
+
+	return (
+		<>
+			<p>
+				If you or your child is interested in studying computer science, USACO is the{' '}
+				<b className="text-white font-medium">best extracurricular activity</b> to pursue.
+			</p>
+
+			{!show && (
+				<button
+					className="inline-flex items-center mt-4 sm:mt-6 text-sm sm:text-base px-3 py-2 sm:px-4 sm:py-3 bg-blueGray-800 hover:bg-blueGray-700 hover:text-white rounded-md font-medium group transition"
+					onClick={() => setShow(true)}
+				>
+					Learn Why
+					<ChevronDownIcon className="ml-1 sm:ml-2 sm:-mr-0.5 h-5 w-5 text-blueGray-400 group-hover:text-blueGray-300 transition" />
+				</button>
+			)}
+
+			{show && (
+				<div className="test">
+					<p className="mt-12 text-white font-medium">
+						First, <span className="text-accent">college applications</span>. USACO provides a major
+						boost in college admissions.
+					</p>
+					<ul className="mt-4 space-y-3">
+						{[
+							'Over 75% of USACO Finalists go to MIT, Stanford, or Harvard',
+							'Platinum is the top ~300 in the nation',
+							'Gold is the top ~750 in the nation'
+						].map((text) => (
+							<li key={text}>
+								<CheckIcon className="absolute h-6 w-5 text-green-400" aria-hidden="true" />
+								<p className="ml-7 leading-6 text-blueGray-300">{text}</p>
+							</li>
+						))}
+					</ul>
+
+					<p className="mt-4">
+						Furthermore, <i>you only need to qualify once</i> for a certain division, and you'll
+						never be demoted to a lower division. Once you make USACO Gold, for example, you're
+						forever in USACO Gold, even if you stop competing in USACO. This is different from math
+						olympiads, where you must constantly practice to make AIME every year.
+					</p>
+
+					<p className="mt-12 text-white font-medium">
+						Second, <span className="text-accent">career growth</span>. USACO prepares you for
+						challenging technical programming interviews you'll encounter later in life.
+					</p>
+
+					<p className="mt-4">
+						The algorithms taught in USACO are the same ones that are tested in programming
+						interviews. By doing USACO, you are preparing yourself to ace these interviews.
+					</p>
+
+					<p className="mt-4">
+						Additionally, criticial thinking and code debugging skills from USACO are applicable in
+						the programming workforce.
+					</p>
+
+					<p className="mt-12 text-white font-medium">
+						In short, doing USACO will set you up to have a successful programming career!
+					</p>
+				</div>
+			)}
+		</>
+	);
+}
--- pages/usaco-classes.tsx
+++ pages/usaco-classes.tsx
@@ -3,6 +3,7 @@ import { CheckIcon } from '@heroicons/react/solid';
 import { CheckIcon as CheckIconOutline } from '@heroicons/react/outline';
 import ClassFeatures from 'components/USACOClasses/ClassFeatures';
 import ClassPricing from 'components/USACOClasses/ClassPricing';
+import WhyUSACO from 'components/USACOClasses/WhyUSACO';
 
 const containerClasses =
 	'py-12 lg:py-16 xl:py-24 2xl:py-32 px-6 md:px-12 lg:px-24 max-w-lg md:max-w-2xl lg:max-w-4xl w-full mx-auto';
@@ -88,56 +89,10 @@ const USACOClassesPage = () => {
 					<div
 						className={`${containerClassesNoYPadding} text-blueGray-300 leading-relaxed sm:text-lg sm:leading-relaxed relative`}
 					>
-						<p>
-							If you or your child is interested in studying computer science, USACO is the{' '}
-							<b className="text-white font-medium">best extracurricular activity</b> to pursue.
-							Why?
-						</p>
+						<WhyUSACO />
 
-						<p className="mt-12 text-white font-medium">
-							First, <span className="text-accent">college applications</span>. USACO provides a
-							major boost in college admissions.
-						</p>
-						<ul className="mt-4 space-y-3">
-							{[
-								'Over 75% of USACO Finalists go to MIT, Stanford, or Harvard',
-								'Platinum is the top ~300 in the nation',
-								'Gold is the top ~750 in the nation'
-							].map((text) => (
-								<li key={text}>
-									<CheckIcon className="absolute h-6 w-5 text-green-400" aria-hidden="true" />
-									<p className="ml-7 leading-6 text-blueGray-300">{text}</p>
-								</li>
-							))}
-						</ul>
-
-						<p className="mt-4">
-							Furthermore, <i>you only need to qualify once</i> for a certain division, and you'll
-							never be demoted to a lower division. Once you make USACO Gold, for example, you're
-							forever in USACO Gold, even if you stop competing in USACO. This is different from
-							math olympiads, where you must constantly practice to make AIME every year.
-						</p>
-
-						<p className="mt-12 text-white font-medium">
-							Second, <span className="text-accent">career growth</span>. USACO prepares you for
-							challenging technical programming interviews you'll encounter later in life.
-						</p>
-
-						<p className="mt-4">
-							The algorithms taught in USACO are the same ones that are tested in programming
-							interviews. By doing USACO, you are preparing yourself to ace these interviews.
-						</p>
-
-						<p className="mt-4">
-							Additionally, criticial thinking and code debugging skills from USACO are applicable
-							in the programming workforce.
-						</p>
-
-						<p className="mt-12 text-white font-medium">
-							In short, doing USACO will set you up to have a successful programming career!
-						</p>
-
-						<hr className="border-blueGray-700 my-12" />
+						{/* <hr className="border-blueGray-700 my-12" /> */}
+            <div className="h-16 sm:h-20"></div>
 
 						<h2 className="font-bold text-2xl sm:text-3xl text-white">About the Instructor</h2>
 
