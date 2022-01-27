785b279cc284628bb155682c0da3878c510a05f3
Nathan Wang <nathan.r.wang@gmail.com>
2022-01-26 18:00:46 -0800

    fix: add division tag properly

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -4,6 +4,13 @@ export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		const { name, email, division } = req.body;
 
+		const divisionToTagID = {
+			Bronze: 2733477,
+			Gold: 2733479,
+			Platinum: 2733480,
+			Silver: 2733478
+		};
+
 		try {
 			// make a fetch POST request to convertkit
 			const resp = await fetch(`https://api.convertkit.com/v3/forms/2738987/subscribe`, {
@@ -15,13 +22,13 @@ export default async function handler(req, res) {
 					api_key: convertkitApiKey,
 					email,
 					name,
-					tags: [division]
+					tags: [divisionToTagID[division]]
 				})
 			});
 			const data = await resp.json();
-      if (!resp.ok) {
-        throw data;
-      }
+			if (!resp.ok) {
+				throw data;
+			}
 			res.status(200).json(data);
 		} catch (e) {
 			console.error(e);
