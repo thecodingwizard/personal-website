2ae79228f173d7daaef9dcfd2c1cfd22a48985bd
Nathan Wang <nathan.r.wang@gmail.com>
2022-09-24 18:33:35 -0400

    update card sizing on small screens

--- components/CardGrid/Card.tsx
+++ components/CardGrid/Card.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import AES from 'crypto-js/aes.js';
 import Utf8 from 'crypto-js/enc-utf8.js';
 
+const cardClasses = 'flex flex-col bg-blue-900 bg-opacity-[15%] p-4 sm:p-6 shadow-md';
+
 export const Card = ({
 	title,
 	link,
@@ -17,14 +19,16 @@ export const Card = ({
 }) => {
 	const content = (
 		<>
-			<h2 className="text-blueGray-300 font-mono font-bold text-xl mb-4 group-hover:text-accent">
+			<h2 className="text-blueGray-300 font-mono font-bold text-lg sm:text-xl mb-2 sm:mb-4 group-hover:text-accent">
 				{title}
 			</h2>
 			{children && (
-				<p className="text-blueGray-400 group-hover:text-blueGray-200 flex-1">{children}</p>
+				<p className="text-blueGray-400 text-sm sm:text-base group-hover:text-blueGray-200 flex-1">
+					{children}
+				</p>
 			)}
 			{tags && (
-				<div className="mt-6 text-sm text-blueGray-500 group-hover:text-blueGray-400 font-mono">
+				<div className="mt-3 sm:mt-6 text-sm text-blueGray-500 group-hover:text-blueGray-400 font-mono">
 					{tags.join(` · `)}
 				</div>
 			)}
@@ -43,7 +47,7 @@ export const Card = ({
 		return (
 			<button
 				onClick={handleClick}
-				className="flex flex-col bg-blue-900 bg-opacity-[15%] hover:bg-opacity-25 group p-6 shadow-md text-left"
+				className={`${cardClasses} hover:bg-opacity-25 group  text-left`}
 			>
 				{content}
 			</button>
@@ -53,24 +57,16 @@ export const Card = ({
 			// internal link
 			return (
 				<Link href={link}>
-					<a className="flex flex-col bg-blue-900 bg-opacity-[15%] hover:bg-opacity-25 group p-6 shadow-md">
-						{content}
-					</a>
+					<a className={`${cardClasses} hover:bg-opacity-25 group`}>{content}</a>
 				</Link>
 			);
 		}
 		return (
-			<a
-				href={link}
-				target="_blank"
-				className="flex flex-col bg-blue-900 bg-opacity-[15%] hover:bg-opacity-25 group p-6 shadow-md"
-			>
+			<a href={link} target="_blank" className={`${cardClasses} hover:bg-opacity-25 group`}>
 				{content}
 			</a>
 		);
 	} else {
-		return (
-			<div className="flex flex-col bg-blue-900 bg-opacity-[15%] p-6 shadow-md">{content}</div>
-		);
+		return <div className={cardClasses}>{content}</div>;
 	}
 };
