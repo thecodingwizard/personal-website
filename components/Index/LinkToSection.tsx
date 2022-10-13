7086d39ff7748ca174e2689e9c8b24adcbaf185e
Nathan Wang <nathan.r.wang@gmail.com>
2022-10-12 20:06:54 -0400

    feat: add gradient border to link to sections

--- components/Index/LinkToSection.tsx
+++ components/Index/LinkToSection.tsx
@@ -1,9 +1,14 @@
 export const LinkToSection = ({ title, href, children }) => (
-	<a href={`#${href}`} className="block sm:text-lg lg:text-xl max-w-prose leading-relaxed group">
-		<span className="group-hover:text-fuchsia-300 text-accent uppercase font-mono">{title}</span>
+	<a
+		href={`#${href}`}
+		className="block sm:text-lg lg:text-xl max-w-prose leading-relaxed group relative"
+	>
+		<span className="group-hover:text-fuchsia-300 text-accent uppercase font-mono transition">
+			{title}
+		</span>
 		<br />
-		<p className="group-hover:text-blueGray-300 text-blueGray-400">
-			{children}
-		</p>
+		<p className="group-hover:text-blueGray-300 text-blueGray-400 transition">{children}</p>
+
+		<span className="absolute w-px -inset-y-3 left-[-1rem] bg-gradient-to-b from-fuchsia-300/0 via-fuchsia-300/60 to-fuchsia-300/0 opacity-0 group-hover:opacity-100 transition"></span>
 	</a>
 );
