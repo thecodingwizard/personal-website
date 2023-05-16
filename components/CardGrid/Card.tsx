60b5717011b13afb2592aeb5436f9194184a7578
Nathan Wang <nathan.r.wang@gmail.com>
2023-05-15 22:14:20 -0400

    add pretty spotlight cards from build ui

--- components/CardGrid/Card.tsx
+++ components/CardGrid/Card.tsx
@@ -3,8 +3,11 @@
 import Link from 'next/link';
 import AES from 'crypto-js/aes.js';
 import Utf8 from 'crypto-js/enc-utf8.js';
+import SpotlightCard from 'components/SpotlightCard';
 
-const cardClasses = 'flex flex-col bg-blue-900 bg-opacity-[15%] p-4 sm:p-6 shadow-md';
+const cardContainerClasses = 'shadow-md rounded-xl shadow-2xl flex flex-col';
+const cardClasses =
+	'flex flex-col p-4 sm:p-6 bg-slate-900 flex-1 rounded-xl border border-white/10';
 
 export const Card = ({
 	title,
@@ -21,16 +24,16 @@ export const Card = ({
 }) => {
 	const content = (
 		<>
-			<h2 className="text-slate-300 font-mono font-bold text-lg sm:text-xl mb-2 sm:mb-4 group-hover:text-accent">
+			<h2 className="text-slate-300 font-mono font-bold text-lg sm:text-xl mb-2 sm:mb-4 group-hover:text-accent transition duration-300">
 				{title}
 			</h2>
 			{children && (
-				<p className="text-slate-400 text-sm sm:text-base group-hover:text-slate-200 flex-1">
+				<p className="text-slate-400 text-sm sm:text-base group-hover:text-slate-200 flex-1 transition duration-300">
 					{children}
 				</p>
 			)}
 			{tags && (
-				<div className="mt-3 sm:mt-6 text-sm text-slate-500 group-hover:text-slate-400 font-mono">
+				<div className="mt-3 sm:mt-6 text-sm text-slate-500 group-hover:text-slate-400 font-mono transition duration-300">
 					{tags.join(` · `)}
 				</div>
 			)}
@@ -47,25 +50,22 @@ export const Card = ({
 			}
 		};
 		return (
-			<button
-				onClick={handleClick}
-				className={`${cardClasses} hover:bg-opacity-25 group  text-left`}
-			>
-				{content}
+			<button onClick={handleClick} className={`${cardContainerClasses} text-left`}>
+				<SpotlightCard className={cardClasses}>{content}</SpotlightCard>
 			</button>
 		);
 	} else if (link && !encrypted) {
 		if (link[0] === '/') {
 			// internal link
 			return (
-				<Link href={link} className={`${cardClasses} hover:bg-opacity-25 group`}>
-					{content}
+				<Link href={link} className={cardContainerClasses}>
+					<SpotlightCard className={cardClasses}>{content}</SpotlightCard>
 				</Link>
 			);
 		}
 		return (
-			<a href={link} target="_blank" className={`${cardClasses} hover:bg-opacity-25 group`}>
-				{content}
+			<a href={link} target="_blank" className={cardContainerClasses}>
+				<SpotlightCard className={cardClasses}>{content}</SpotlightCard>{' '}
 			</a>
 		);
 	} else {
---
+++ components/SpotlightCard.tsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import { motion, useMotionTemplate, useMotionValue } from 'framer-motion';
+import { MouseEvent } from 'react';
+
+export default function SpotlightCard({
+	children,
+	className = 'rounded-xl border border-white/10 bg-slate-900 shadow-2xl'
+}) {
+	let mouseX = useMotionValue(0);
+	let mouseY = useMotionValue(0);
+
+	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+		let { left, top } = currentTarget.getBoundingClientRect();
+
+		mouseX.set(clientX - left);
+		mouseY.set(clientY - top);
+	}
+
+	return (
+		<div
+			// className="group relative max-w-md border border-white/10 bg-gray-900 px-8 py-16 shadow-2xl"
+			className={classNames('group relative', className)}
+			onMouseMove={handleMouseMove}
+		>
+			<motion.div
+				className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
+				style={{
+					background: useMotionTemplate`
+            radial-gradient(
+              450px circle at ${mouseX}px ${mouseY}px,
+              rgba(14, 165, 233, 0.075),
+              transparent 80%
+            )
+          `
+				}}
+			/>
+			{children}
+		</div>
+	);
+}
