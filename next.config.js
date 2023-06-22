96468792ae2e038c75b25466ca434c5eb4a7f49a
Nathan Wang <nathan.r.wang@gmail.com>
2023-06-22 00:16:31 -0700

    fix rewrite

--- next.config.js
+++ next.config.js
@@ -10,7 +10,21 @@ module.exports = {
   },
   async rewrites() {
     return {
+      // We need this rewrite in beforeFiles, otherwise
+      // tcw.sh will still serve the normal homepage
       beforeFiles: [
+        {
+          source: '/',
+          has: [
+            {
+              type: 'host',
+              value: 'tcw.sh',
+            },
+          ],
+          destination: '/l/',
+        },
+      ],
+      fallback: [
         {
           source: '/:path*',
           has: [
